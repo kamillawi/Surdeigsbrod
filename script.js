b6bb89493bf7fd991c36e89ec0725fbefea0467a
kamillawi <60691975+kamillawi@users.noreply.github.com>
2025-03-12 13:07:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let starterInput = document.getElementById("starter");
+
+function visVerdi() {
+    let gramSurdeigsstarter = starterInput.value;
+
+    vann.textContent = Math.round(gramSurdeigsstarter * 1.44);
+    mel.textContent = Math.round(gramSurdeigsstarter * 2.5);
+
+
+    
+}
+
+
+
+starterInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        visVerdi(); // Kall på visVerdi-funksjonen når Enter trykkes
+    }
+});
