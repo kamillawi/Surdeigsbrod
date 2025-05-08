338601ed5dd360557492693feea3c3a1a8a626e4
kamillawi <60691975+kamillawi@users.noreply.github.com>
2025-05-08 13:27:03 +0200

    Update script.js
    Oppdatert med knapp

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let starterInput = document.getElementById("starter");
 function visVerdi() {
     let gramSurdeigsstarter = starterInput.value;
 
-    vann.textContent = Math.round(gramSurdeigsstarter * 1.44) + " g vann";
-    mel.textContent = Math.round(gramSurdeigsstarter * 2.5) + " g mel";
+    vann.textContent = Math.round(gramSurdeigsstarter * 1.44) + "g vann";
+    mel.textContent = Math.round(gramSurdeigsstarter * 2.5) + "g mel";
 
 
     
@@ -19,3 +19,8 @@ starterInput.addEventListener("keypress", function(event) {
         visVerdi(); // Kall på visVerdi-funksjonen når Enter trykkes
     }
 });
+
+knapp.addEventListener("click", function() {
+    visVerdi(); // Call visVerdi when the button is clicked
+});
+
